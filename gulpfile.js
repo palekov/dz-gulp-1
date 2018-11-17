ed2e0febd605fb9ca3ffaa6fc09c20c0c20604c4
Alexander <palekov-2011@mail.ru>
2018-11-17 09:55:27 +0300

    fixed some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var less = require('gulp-less');
-//var sass = require('gulp-sass');
 
 gulp.task('server', ['styles'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-     gulp.watch('./app/less/**/*.less', ['less']);
-    //gulp.watch('./app/sass/**/*.scss', ['sass']);
+     gulp.watch('./app/less/**/*.less', ['styles']);
 });
 
 gulp.task('styles', function() {
